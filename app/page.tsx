62143c72b9709c6e65804c80b53a650c885b4cd2
gustafer <gustavobreasy@gmail.com>
2024-11-08 12:37:31 -0300

    feat: create plans, fix: routines was mapping the plans

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,14 @@
 
 import { baseUrlRoute } from "@/api/lib/routes";
 import { getCookie } from "cookies-next";
-import { Calendar, Loader2, Notebook } from "lucide-react";
+import { Calendar, DollarSign, Loader2, Notebook, Timer } from "lucide-react";
 import { useEffect, useState } from "react";
+
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  trailingZeroDisplay: 'stripIfInteger' 
+});
 
 export default function Page() {
   const authToken = getCookie("auth");
@@ -93,14 +99,24 @@ export default function Page() {
                   <Notebook />
                   Plans:
                 </h1>
-                <div className="flex flex-row flex-wrap items-center justify-center gap-4">
+                <div className="flex flex-row flex-wrap justify-center items-center gap-4">
                   {data.plans.map((plan, i) => (
                     <div
-                      className="border border-muted p-2 rounded-md flex flex-col justify-center items-center"
+                      className="border border-muted p-2 rounded-md flex flex-col justify-center items-center gap-2"
                       key={i}
                     >
                       <h1 className="font-semibold">{plan.name}</h1>
                       <p className="text-sm">{plan.description}</p>
+                      <div className="flex flex-row items-center gap-4 flex-wrap justify-center">
+                        <div className="flex flex-col items-center justify-center border border-muted rounded-md p-2">
+                          <h2 className="font-semibold border-b border-muted text-center w-16 flex flex-row gap-2 items-center"><DollarSign className="w-4" />Price</h2>
+                          <p className="text-sm">{formatter.format(plan.price)}</p>
+                        </div>
+                        <div className="flex flex-col items-center justify-center border border-muted rounded-md p-2">
+                          <h2 className="font-semibold border-b border-muted text-center w-18 flex flex-row gap-2 items-center"><Timer className="w-4"/>Duration</h2>
+                          <p className="text-sm">{plan.duration} Days</p>
+                        </div>
+                      </div>
                       {/* eslint-disable-next-line @next/next/no-img-element */}
                       <img
                         src={plan.img}
@@ -117,8 +133,8 @@ export default function Page() {
                 <h1 className="text-xl font-semibold flex flex-row gap-2 items-center justify-center">
                   <Calendar /> Routines:
                 </h1>
-                <div className="flex flex-row flex-wrap items-center justify-center gap-4">
-                  {data.plans.map((routines, i) => (
+                <div className="flex flex-row flex-wrap  gap-4">
+                  {data.routines.map((routines, i) => (
                     <div
                       className="border border-muted p-2 rounded-md flex flex-col justify-center items-center"
                       key={i}
