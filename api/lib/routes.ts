1767a10656d51367dfaedfaf52ab910a89d5f66f
gustafer <gustavobreasy@gmail.com>
2025-01-02 18:10:36 -0300

    fix: add env config for axios baseurl (dynamic hosting)

--- api/lib/routes.ts
+++ api/lib/routes.ts
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-const API_BASE_URL = "http://localhost:8000"
-
 export const baseUrlRoute = axios.create({
-    baseURL: API_BASE_URL
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL
 })
